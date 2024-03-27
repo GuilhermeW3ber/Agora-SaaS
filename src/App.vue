719b6401d795c2a0d4bc40ffa8f4a22789549ebd
Guilherme Weber | Ágora <95313321+GuilhermeW3ber@users.noreply.github.com>
2024-03-26 23:10:00 -0300

    feat: login structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
-  import HomePage from './components/HomePage.vue'
+  // import HomePage from './components/HomePage.vue'
+  import LoginPage from './components/Pages/LoginPage.vue';
 
 
   export default {
     name: 'App',
     components: {
-      HomePage,
+      // HomePage,
+      LoginPage,
     }
   }
 </script>
 
 <template>
   <div>
-    <HomePage msg="Welcome to Your Vue.js App"/>
+    <!-- <HomePage msg="Welcome to Your Vue.js App"/> -->
+    <LoginPage/>
   </div>
 </template>
 
---
+++ src/components/Pages/LoginPage.vue
@@ -0,0 +1,42 @@
+<script>
+
+// import NavbarMenu from '../Navbar/NavbarMenu.vue'
+
+export default {
+  name: 'LoginPage',
+  components: {
+    // NavbarMenu,
+  },
+  props: {
+    msg: String
+  }
+  
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="input-container">
+        <h1 id="title">Bem vindo ao flow</h1>
+        <h2 id="sub-title">Entre com sua conta.</h2>
+        <h2 id="input-name">E-Mail</h2>
+        <input type="text" background-image=""  placeholder="Seu E-Mail">
+        <h2>Senha</h2>
+        <input type="text" placeholder="Sua Senha">
+        <div class="login-container">
+          <div class="button-container">
+            <a id="submit-button" href="">
+              <p>Entrar</p>
+            </a>
+          </div>
+          <div class="login-text-container">
+            <h3>Não tem uma conta? <a href="">Cadastre-se.</a></h3>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  @import '../../styles.scss';
+</style>
